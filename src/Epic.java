bed506db46463a077d750c04b1aa6ad9cb5f525a
Tozier <java_Yap@icloud.com>
2024-06-24 20:26:10 +0400

    С нуля заново

--- src/Epic.java
+++ src/Epic.java
@@ -4,31 +4,29 @@ import java.util.HashMap;
 public class Epic extends Task {
     HashMap<Integer, SubTask> epicSubList = new HashMap<>();
 
-    Epic(String taskName, String content, Status status) {
+    Epic(String taskName, String content,  Status status) {
         super(taskName, content, status);
     }
 
     void addSubTask(SubTask subTask) {
         epicSubList.put(subTask.getId(), subTask);
     }
-
-    void printAllSubList() {
+    void printAllSubList(){
         for (SubTask subTask : epicSubList.values()) {
             System.out.println("Obj: " + subTask);
         }
     }
-
-    void statusUpdate() {
-        if (epicSubList.isEmpty()) {
+    void statusUpdate (){
+        if(epicSubList.isEmpty()){
             setStatus(Status.NEW);
             return;
-        } else {
-            for (SubTask subTask : epicSubList.values()) {
-                if (subTask.getStatus() == Status.IN_PROGRESS) {//Хотя бы один In Progress
+        }else{
+            for (SubTask subTask : epicSubList.values()){
+                if(subTask.getStatus()==Status.IN_PROGRESS){//Хотя бы один In Progress
                     setStatus(Status.IN_PROGRESS);
                     return;
-                } else if (subTask.getStatus() == Status.NEW) {
-                    return;
+                } else if (subTask.getStatus()==Status.NEW){
+                   return;
                 } else {
                     setStatus(Status.DONE);
                 }
@@ -36,8 +34,7 @@ public class Epic extends Task {
         }
 
     }
-
-    void clearSub() {
+    void clearSub(){
         epicSubList.clear();
         setStatus(Status.NEW);
     }
